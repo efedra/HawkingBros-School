506ff2418e09bc2b79dd288ccf747820c8864f82
efedra <efedra-00@mail.ru>
2022-11-07 20:39:31 +0300

    Done homework 1.2

--- HawkingBros1.2/script.js
+++ HawkingBros1.2/script.js
@@ -1,26 +1,34 @@
 //Удалить из массива дубликаты
-let arr = [1,2,3,5,1,6,2,5,6,7]
+let arr = [1, 2, 3, 5, 1, 6, 2, 5, 6, 7]
+let set = [...new Set(arr)]
+console.log(`1) ${set}`)
 //Объединить 2 объекта в один
 let obj1 = {
-    id:1,
-    name:'userName'
+    id: 1,
+    name: 'userName'
 }
 
 let obj2 = {
-    id:1,
-    name:'qwerty'
+    id: 1,
+    name: 'qwerty'
 }
-
+let objUnion = {...obj1, ...obj2}
+console.log(`2) ${objUnion.id}, ${objUnion.name}`)
 //Сократите объявление функции в одну строку с помощью ES6
-let add = function (x,y){
-    return x+y
+let add = function (x, y) {
+    return x + y
 }
+let addShort = (x, y) => x + y
+console.log(`3) ${addShort(4, 5)}`)
 
-//Напишите функцию, с помощью которой можно будет выполнять различные действия с зажержкой
+//Напишите функцию, с помощью которой можно будет выполнять различные действия с задержкой
 
-let sleep = ms => {}
+let sleep = (ms) =>  new Promise((resolve) => {
+    setTimeout(()=>resolve(ms),ms);
+
+})
 
 //пример использования
-sleep(5000)(()=>{
-    console.log('Выполнилось через ')
+sleep(5000).then((value)=>{
+    console.log(`4) Выполнилось через ${value / 1000} секунд`)
 })

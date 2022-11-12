94de04738ea68ad1606a782c65e3877a877f060b
efedra <efedra-00@mail.ru>
2022-11-12 13:37:31 +0300

    Done 2.2

--- HawkingBros2.1/axios.js
+++ HawkingBros2.1/axios.js
@@ -1,4 +1,5 @@
 import axios from 'axios';
+
 const loadBtn = document.querySelector(".js-load-axios");
 const resultsContainer = document.querySelector(".js-results");
 const searchInput = document.querySelector(".js-input-axios");
@@ -7,15 +8,20 @@ loadBtn.addEventListener("click", function (evt) {
     evt.preventDefault();
 
     const searchValue = searchInput.value.trim().toLowerCase();
-    axios.get(`https://jsonplaceholder.typicode.com/users${searchValue}`)
+    axios.get(`https://jsonplaceholder.typicode.com/users/${searchValue}`)
         .then(res => {
             console.log(res)
             resultsContainer.innerHTML = `<div class="response-container">
-                <img src="${res.avatar_url}">
-                <p> Имя: <span>${res.name}</span><p>
-                <p> О себе: <span>${res.bio}</span><p>
-                <p> Кол-во репозиториев: <span>${res.public_repos}</span><p>
+                <p> Никнейм: <span>${res.data.username}</span><p>
+                <p> Имя: <span>${res.data.name}</span><p>
+                <p> e-mail: <span>${res.data.email}</span><p>
+                <p> Телефон: <span>${res.data.phone}</span><p>
+                <p> Вебсайт: <span>${res.data.website}</span><p>
             </div>`
-        }).catch()
+        }).catch((e)=>{
+        (resultsContainer.innerHTML = `<div class="response-container">
+                <p>Такого пользователя не существует. Поиск происходит по id.<p>
+            </div>`)
+    })
 
 });
---
+++ dist/index.d64bea41.js
@@ -0,0 +1,24 @@
+const loadBtn = document.querySelector(".js-load");
+const resultsContainer = document.querySelector(".js-results");
+const searchInput = document.querySelector(".js-input");
+loadBtn.addEventListener("click", function(evt) {
+    evt.preventDefault();
+    const searchValue = searchInput.value.trim().toLowerCase();
+    fetch(`https://api.github.com/users/${searchValue}`).then((response)=>{
+        if (response.ok) return response.json();
+        else throw "Такого пользователя не существует =(";
+    }).then((data)=>{
+        resultsContainer.innerHTML = `<div class="response-container">
+                <img src="${data.avatar_url}">
+                <p> Имя: <span>${data.name}</span><p>
+                <p> О себе: <span>${data.bio}</span><p>
+                <p> Кол-во репозиториев: <span>${data.public_repos}</span><p>
+            </div>`;
+    }).catch((e)=>{
+        resultsContainer.innerHTML = `<div class="response-container">
+                <p>${e}<p>
+            </div>`;
+    });
+});
+
+//# sourceMappingURL=index.d64bea41.js.map
